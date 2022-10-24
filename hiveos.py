ebe6b686556cccd1ff275098525a3e97f6bb3797
mikohir <82265726+mikohir@users.noreply.github.com>
2022-10-24 12:09:07 +0300

    Add files via upload

--- hiveos.py
+++ hiveos.py
@@ -1,4 +1,5 @@
 from requests import get
+from math import log, floor
 
 #File with sensitive information needed for the script
 import credentials
@@ -24,28 +25,62 @@ def hiveos_requests():
     hiveos_api_keys = hiveos_api.json()
     hiveos_api_keys.keys()
 
-    #Worker 1 GPU 1 data
-    worker_1_gpu_1_name = "Radeon RX 5700 MSI Mech: "
-    worker_1_gpu_1_hashrate = hiveos_api_keys["gpu_stats"][0]["hash"]
-    worker_1_gpu_1_power = hiveos_api_keys["gpu_stats"][0]["power"]
-    worker_1_gpu_1_fan = hiveos_api_keys["gpu_stats"][0]["power"]
-    worker_1_gpu_1_coretemp = hiveos_api_keys["gpu_stats"][0]["temp"]
-    worker_1_gpu_1_memtemp = hiveos_api_keys["gpu_stats"][0]["memtemp"]
+    #Function for changing large numbers to Kilos, Megas etc.
+    def hashrate_format(number):
+        units = ['', ' MH', ' GH', ' TH', ' PH']
+        k = 1000.0
+        magnitude = int(floor(log(number, k)))
+        return '%.2f%s' % (number / k**magnitude, units[magnitude])
 
-    worker_1_gpu_1_stats = [worker_1_gpu_1_name, worker_1_gpu_1_hashrate, worker_1_gpu_1_coretemp, worker_1_gpu_1_memtemp, worker_1_gpu_1_fan, worker_1_gpu_1_power] 
+    def worker_1():
 
-    #Worker 1 GPU 2 data
-    worker_1_gpu_2_name = "Radeon RX 5700 Gigabyte OC: "
-    worker_1_gpu_2_hashrate = hiveos_api_keys["gpu_stats"][1]["hash"]
-    worker_1_gpu_2_power = hiveos_api_keys["gpu_stats"][1]["power"]
-    worker_1_gpu_2_fan = hiveos_api_keys["gpu_stats"][1]["power"]
-    worker_1_gpu_2_coretemp = hiveos_api_keys["gpu_stats"][1]["temp"]
-    worker_1_gpu_2_memtemp = hiveos_api_keys["gpu_stats"][1]["memtemp"]
+        #Worker 1 GPU 0 data
+        worker_1_gpu_0_name = "GPU 0: Radeon RX 5700 MSI Mech: "
+        worker_1_gpu_0_hashrate = hiveos_api_keys["gpu_stats"][0]["hash"]
+        worker_1_gpu_0_power = str(hiveos_api_keys["gpu_stats"][0]["power"]) + " W"
+        worker_1_gpu_0_fan = str(hiveos_api_keys["gpu_stats"][0]["fan"]) + " %"
+        worker_1_gpu_0_coretemp = str(hiveos_api_keys["gpu_stats"][0]["temp"]) + " °C"
+        worker_1_gpu_0_memtemp = str(hiveos_api_keys["gpu_stats"][0]["memtemp"]) + " °C"
 
-    worker_1_gpu_2_stats = [worker_1_gpu_2_name, worker_1_gpu_2_hashrate, worker_1_gpu_2_coretemp, worker_1_gpu_2_memtemp, worker_1_gpu_2_fan, worker_1_gpu_2_power]
+        worker_1_gpu_0_stats = [worker_1_gpu_0_name, hashrate_format(worker_1_gpu_0_hashrate), worker_1_gpu_0_coretemp, worker_1_gpu_0_memtemp, worker_1_gpu_0_fan, worker_1_gpu_0_power] 
 
+        #Worker 1 GPU 1 data
+        worker_1_gpu_1_name = "GPU 1: Radeon RX 5700 Gigabyte OC: "
+        worker_1_gpu_1_hashrate = hiveos_api_keys["gpu_stats"][1]["hash"]
+        worker_1_gpu_1_power = str(hiveos_api_keys["gpu_stats"][1]["power"]) + " W"
+        worker_1_gpu_1_fan = str(hiveos_api_keys["gpu_stats"][1]["fan"]) + " %"
+        worker_1_gpu_1_coretemp = str(hiveos_api_keys["gpu_stats"][1]["temp"]) + " °C"
+        worker_1_gpu_1_memtemp = str(hiveos_api_keys["gpu_stats"][1]["memtemp"]) + " °C"
 
-    print(worker_1_gpu_1_stats, worker_1_gpu_2_stats)
+        worker_1_gpu_1_stats = [worker_1_gpu_1_name, hashrate_format(worker_1_gpu_1_hashrate), worker_1_gpu_1_coretemp, worker_1_gpu_1_memtemp, worker_1_gpu_1_fan, worker_1_gpu_1_power]
+
+        #Worker 1 GPU 2 data
+        worker_1_gpu_2_name = "GPU 2: Radeon RX 5700 Powercolor Red Dragon: "
+        worker_1_gpu_2_hashrate = hiveos_api_keys["gpu_stats"][2]["hash"]
+        worker_1_gpu_2_power = str(hiveos_api_keys["gpu_stats"][2]["power"]) + " W"
+        worker_1_gpu_2_fan = str(hiveos_api_keys["gpu_stats"][2]["fan"]) + " %"
+        worker_1_gpu_2_coretemp = str(hiveos_api_keys["gpu_stats"][2]["temp"]) + " °C"
+        worker_1_gpu_2_memtemp = str(hiveos_api_keys["gpu_stats"][2]["memtemp"]) + " °C"
+
+        worker_1_gpu_2_stats = [worker_1_gpu_2_name, hashrate_format(worker_1_gpu_2_hashrate), worker_1_gpu_2_coretemp, worker_1_gpu_2_memtemp, worker_1_gpu_2_fan, worker_1_gpu_2_power]
+
+        #Worker 1 GPU 3 data
+        worker_1_gpu_3_name = "GPU 3: Radeon RX 5700 ASUS Strix: "
+        worker_1_gpu_3_hashrate = hiveos_api_keys["gpu_stats"][3]["hash"]
+        worker_1_gpu_3_power = str(hiveos_api_keys["gpu_stats"][3]["power"]) + " W"
+        worker_1_gpu_3_fan = str(hiveos_api_keys["gpu_stats"][3]["fan"]) + " %"
+        worker_1_gpu_3_coretemp = str(hiveos_api_keys["gpu_stats"][3]["temp"]) + " °C"
+        worker_1_gpu_3_memtemp = str(hiveos_api_keys["gpu_stats"][3]["memtemp"]) + " °C"
+
+        worker_1_gpu_3_stats = [worker_1_gpu_3_name, hashrate_format(worker_1_gpu_3_hashrate), worker_1_gpu_3_coretemp, worker_1_gpu_3_memtemp, worker_1_gpu_3_fan, worker_1_gpu_3_power]
+
+        #Create a list of all the different GPUs
+        gpu_list = [worker_1_gpu_0_stats, worker_1_gpu_1_stats, worker_1_gpu_2_stats, worker_1_gpu_3_stats]
+
+        #Print out the GPU list in seperate lines
+        print(*gpu_list, sep="\n")
+
+    worker_1()
 
 
 if __name__ == '__main__':
